3f852c71dbfcd75316c66f1ed1f3deb6e4d0d6dd
meayushraj <ayushrajdav@gmail.com>
2024-02-24 16:47:41 +0530

    deleteNodeByValue method added

--- codeSnippet/src/com/datastructures/linkedlist/DataStructuresMain.java
+++ codeSnippet/src/com/datastructures/linkedlist/DataStructuresMain.java
@@ -1,18 +1,28 @@
 package com.datastructures.linkedlist;
 
 public class DataStructuresMain {
-  public static void main(String[] args) {
-    Node node = new Node(1234567890);
-    System.out.println(
-        "Path --> " + node + " | Node --> " + node.value + " | Value --> " + node.value + "\n");
+    public static void main(String[] args) {
+        Node node = new Node(1234567890);
+        System.out.println("Path --> " + node + " | Node --> " + node.value + " | Value --> " + node.value + "\n");
 
-    SingleLinkedList singleLinkedList = new SingleLinkedList(1);
-    System.out.println("SingleLinkedList Initial Value " + singleLinkedList.head);
+        SingleLinkedList singleLinkedList = new SingleLinkedList(1);
+        System.out.println("SingleLinkedList Initial Value " + singleLinkedList.head);
 
-    singleLinkedList.insertAtEnd(2);
-    singleLinkedList.insertAtEnd(3);
-    singleLinkedList.insertAtEnd(5);
-    singleLinkedList.insertAtBeginning(0);
-    singleLinkedList.printNode();
-  }
+        singleLinkedList.insertAtEnd(2);
+        singleLinkedList.insertAtEnd(3);
+        singleLinkedList.insertAtEnd(101);
+        singleLinkedList.insertAtEnd(1001);
+        singleLinkedList.insertAtEnd(4);
+        singleLinkedList.insertAtEnd(5);
+        singleLinkedList.insertAtBeginning(0);
+        singleLinkedList.deleteNodeByValue(0);
+
+        singleLinkedList.deleteNodeByIndex(2);
+
+
+//    singleLinkedList.search(0);
+//    singleLinkedList.insertAfter(0);
+//    singleLinkedList.insertAfter(0);
+        singleLinkedList.printNode();
+    }
 }
--- codeSnippet/src/com/datastructures/linkedlist/SingleLinkedList.java
+++ codeSnippet/src/com/datastructures/linkedlist/SingleLinkedList.java
@@ -1,28 +1,61 @@
 package com.datastructures.linkedlist;
 
 public class SingleLinkedList {
-  Node head;
+    Node head;
 
-  public SingleLinkedList(int i) {
-    this.head = new Node(i);
-  }
-
-  public void printNode() {
-    while (this.head != null) {
-      System.out.println(this.head.value);
-      this.head = this.head.next;
+    public SingleLinkedList(int i) {
+        this.head = new Node(i);
     }
-  }
 
-  public void insertAtEnd(int i) {
-    Node last = head;
-    while (last.next != null) last = last.next;
-    last.next = new Node(i);
-  }
+    public void printNode() {
+        while (this.head != null) {
+            System.out.println(this.head.value);
+            this.head = this.head.next;
+        }
+    }
 
-  public void insertAtBeginning(int i) {
-    Node new_node = new Node(i);
-    new_node.next = head;
-    head = new_node;
-  }
+    public void insertAtEnd(int i) {
+        Node last = head;
+        while (last.next != null) last = last.next;
+        last.next = new Node(i);
+    }
+
+    public void insertAtBeginning(int i) {
+        Node new_node = new Node(i);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public void insertAfter(int i) {
+    }
+
+    public void deleteNodeByValue(int value) {
+
+        if (head.value == value) {
+            head = head.next;
+        }
+
+        boolean breakFlag = true;
+        Node prev = head;
+        Node curr = head.next;
+
+        while (curr.next != null && breakFlag) {
+            if (curr.value == value) {
+                prev.next = curr.next;
+                breakFlag = false;
+            } else {
+                prev = prev.next;
+                curr = curr.next;
+            }
+
+        }
+    }
+
+    public void deleteNodeByIndex(int index) {
+    }
+
+    public boolean search(int i) {
+
+        return true;
+    }
 }
